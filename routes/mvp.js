ded7e6ef350af92178fc12e0e475aeb562465524
Spencer <spencer.meredith4@gmail.com>
2021-10-14 01:20:14 -0600

    database setup and integration
    I've opted to use MySQL for my database. I've added a .env configuration file to store the MySQL credentials and other database information and I've added a file to store all functions that interface with MySQL directly, which includes functions for getting the reviews, adding a review, and calculating the average rating for a given review table. The MVP router used by express has been updated to use the database integration and all static data has been removed. Added a "/reset" route to the MVP router to allow me to reset the database to its initial state after testing. This route is not exposed to the MVP web page.

--- routes/mvp.js
+++ routes/mvp.js
@@ -1,38 +1,43 @@
 const express = require("express")
 const router = express.Router()
 
-let reviews = [
-	{ rating: 4, review: "book was full of fluff" },
-	{ rating: 3, review: "book was fluff" },
-	{ rating: 4, review: "book was amazing" }
-]
+const {
+	initialize,
+	addReview,
+	getReviews,
+	getAverageRating
+} = require("../database/mysql")
+const TABLE = "reviews_mvp"
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
 	console.log("retrieving MVP reviews")
 	// retrieve all reviews from the MVP reviews table
 	// and calculate the average rating
-	res.json({
-		averageRating: calculateAverageRating(),
-		reviews
-	})
+	let results = await getReviewsAndAverageRating()
+	res.json(results)
 })
 
-router.post("/addReview", (req, res) => {
+router.post("/addReview", async (req, res) => {
 	// take body and add review to MVP reviews table
 	console.log("adding MVP review")
-	reviews.push(req.body)
-	res.json({
-		averageRating: calculateAverageRating(),
-		reviews
-	})
+	await addReview(TABLE, req.body.rating, req.body.review)
+	let results = await getReviewsAndAverageRating()
+	res.json(results)
 })
 
-function calculateAverageRating() {
-	const averageRating =
-		reviews.reduce((total, current) => {
-			return total + current.rating
-		}, 0) / reviews.length
-	return Math.round(averageRating * 10) / 10
+async function getReviewsAndAverageRating() {
+	let results = await Promise.all([getAverageRating(TABLE), getReviews(TABLE)])
+	return {
+		averageRating: results[0],
+		reviews: results[1]
+	}
 }
+
+router.get("/reset", async (req, res) => {
+	// re-initialize the database tables
+	console.log("resetting the MVP reviews table")
+	await initialize(TABLE)
+	res.status(200).send("reset complete")
+})
 
 module.exports = router
